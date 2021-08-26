6abebd396cbc0edc3e9b06cb2e3b49253302d9e3
Abhishek-Raj-Poudel <89299685+Abhishek-Raj-Poudel@users.noreply.github.com>
2021-08-26 08:30:10 +0545

    Created a react App
    First Commit and just now created the react app and cleaned it up

---
+++ spotify-clone-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Everybody lets make a Spotify Clone!!!</h1>
+    </div>
+  );
+}
+
+export default App;
