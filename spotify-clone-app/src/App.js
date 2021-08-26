e67a5beaecb7387588c472e15ff1c755b4501fac
Abhishek-Raj-Poudel <89299685+Abhishek-Raj-Poudel@users.noreply.github.com>
2021-08-26 09:15:34 +0545

    Basic Spotify Login Page
    No function just a page layout which has not been complete

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Everybody lets make a Spotify Clone!!!</h1>
+    <div className="app">
+      <Login />
     </div>
   );
 }
