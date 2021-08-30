764a87e2ab6753d5d716287d3c18b4006f828757
Abhishek-Raj-Poudel <89299685+Abhishek-Raj-Poudel@users.noreply.github.com>
2021-08-30 08:03:22 +0545

    Made User Authentication
    And can also get a token for a user as well and if we have that token then we are logged in

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,12 +1,31 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState();
+  //Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("Me", user);
+      });
+    }
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
